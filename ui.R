a4bc1fe164e3215e5cb04fbc265c1d82710bce39
FvD <frans@ixpantia.com>
2022-10-29 10:57:11 -0600

    cambio para ensayar actualización

--- ui.R
+++ ui.R
@@ -5,7 +5,7 @@ library(shiny)
 # Encabezado --------------------------------------------------------------
 
 header <- dashboardHeader(
-  title = "Módulos"
+  title = "Módulos cambio"
 )
 
 # Sidebar -----------------------------------------------------------------
